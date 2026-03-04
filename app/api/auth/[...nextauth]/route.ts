d9e50964e8deee841ea9f1aed611b8a7122f36bb
mahider AB <mahamahderr57@gmail.com>
2026-03-03 23:35:14 -0800

    add the prisma adapter

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/prisma/client";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
--- app/issues/new/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import IssueForm from "../_components/IssueForm";
-
-const NewIssueForm = () => {
-  return <IssueForm />;
-};
-
-export default NewIssueForm;
